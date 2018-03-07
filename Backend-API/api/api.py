0da2b527d83079541e94de72fb2f08494fb45719
Huw Jones <huwcbjones@gmail.com>
2018-03-07 15:45:21 +0000

    Created basic API

---
+++ Backend-API/api/api.py
@@ -0,0 +1,209 @@
+import json
+import logging
+import socket
+import asyncio
+import inspect
+from enum import Enum
+from json import JSONDecodeError
+from threading import Thread
+
+import struct
+import tornado.web
+
+from api import controllers
+
+
+class API:
+    app = None
+
+    def __init__(self, enable_binary, binary_port, binary_address, enable_rest, rest_port, rest_address):
+        if API.app is None:
+            API.app = self
+        else:
+            raise RuntimeError()
+        if __debug__:
+            logging.warning("DEBUG MODE IS ENABLED!")
+            logging.warning("Safeguards may be disabled.")
+            logging.warning("To disable this warning, run python with the -O flag")
+
+        self.enable_binary = enable_binary
+        self.enable_rest = enable_rest
+
+        if enable_binary:
+            try:
+                self._check_ports(binary_address, binary_port)
+                self._init_binary(binary_address, binary_port)
+            except OSError as e:
+                logging.fatal("Error binding to {}:{}".format(binary_address, binary_port))
+                logging.fatal(e.strerror)
+                quit(1)
+        if enable_rest:
+            try:
+                self._check_ports(rest_address, rest_port)
+                self._init_rest(rest_address, rest_port)
+            except OSError as e:
+                logging.fatal("Error binding to {}:{}".format(rest_address, rest_port))
+                logging.fatal(e.strerror)
+                quit(1)
+        pass
+
+    def _init_binary(self, address, port):
+        self._binary_api_thread = Thread(
+            name="binary_api_thread",
+            target=self._create_binary_thread,
+            args=(address, port),
+            daemon=True
+        )
+
+    def _init_rest(self, address, port):
+        logging.info("Loading controllers...")
+
+        routes = self._load_routes(controllers, set())
+
+        logging.info("Loaded {} routes!".format(len(routes)))
+
+        class DefaultController(controllers.Controller):
+
+            def prepare(self):
+                raise tornado.web.HTTPError(status_code=404)
+
+        routes.append((r"[\w\W]*", DefaultController))
+        app = tornado.web.Application(routes)
+        app.listen(port=port, address=address)
+
+    def _create_binary_thread(self, address, port):
+        self.binary_api_loop = asyncio.new_event_loop()
+        loop = self.binary_api_loop
+        logging.debug("Creating Binary API")
+        self._binary_api = loop.create_server(
+            BinaryProtocol,
+            address, port
+        )
+        logging.info("Starting Binary API...")
+        loop.run_until_complete(self._binary_api)
+        loop.run_forever()
+        loop.close()
+
+    def _load_routes(self, obj, checked):
+        routes = []
+        # Loop through members of obj
+        for name, obj in inspect.getmembers(obj):
+            if name in checked:
+                continue
+            checked.add(name)
+            if name[0:2] == '__':
+                continue
+
+            if inspect.ismodule(obj):
+                routes.extend(self._load_routes(obj, checked))
+                continue
+
+            if not inspect.isclass(obj):
+                continue
+
+            if not issubclass(obj, (controllers.Controller, controllers.WebSocketController)):
+                continue
+
+            logging.debug("Checking {}".format(name))
+            if obj.route is not None:
+                [routes.append((regex, obj)) for regex in obj.route]
+                logging.debug("Loaded {} routes for {}!".format(len(obj.route), name))
+
+        return routes
+
+    def run(self):
+        """
+        Run the server
+        """
+        logging.info("Starting listen loop...")
+        if self.enable_binary:
+            self._binary_api_thread.start()
+
+        loop = asyncio.get_event_loop()
+        try:
+            loop.run_forever()
+        except:
+            # Find all running tasks:
+            pending = asyncio.Task.all_tasks()
+
+            # Run loop until tasks done:
+            loop.run_until_complete(asyncio.gather(*pending))
+        finally:
+            loop.stop()
+            loop.close()
+
+    @staticmethod
+    def _check_ports(address, port):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((address, port))
+        s.close()
+
+
+class BinaryProtocol(asyncio.Protocol):
+
+    def __init__(self):
+        self.transport = None
+        self.remote = None
+        self.remote_address = None
+        self.remote_port = None
+        pass
+
+    def data_received(self, data):
+        logging.debug("RECV {} <- {}".format(data, self.remote))
+        try:
+            destination, data = decode_payload(data)
+            logging.debug("Destination: {}".format(destination))
+            logging.debug("Payload: {}".format(data))
+        except DecodeError as e:
+            logging.error(e)
+            return
+
+        # Do stuff
+
+        super().data_received(data)
+
+    def eof_received(self):
+        logging.info("Received disconnect from {}".format(self.remote))
+        super().eof_received()
+
+    def connection_made(self, transport):
+        self.transport = transport
+        if self.remote_address is None:
+            sockname = transport.get_extra_info('socket').getpeername()
+            self.remote_address = sockname[0]
+            self.remote_port = sockname[1]
+            self.remote = self.remote_address + ":" + str(self.remote_port)
+        logging.info("Connected to {}".format(self.remote))
+
+    def connection_lost(self, exc):
+        logging.info("Lost connection to {}")
+        super().connection_lost(exc)
+
+    def write_data(self, data):
+        logging.debug("SENT {} -> {}".format(data, self.remote))
+        self.transport.write(data)
+
+
+def get_payload(destination, data=None):
+    payload = "".encode()
+    if data is not None:
+        payload = json.dumps(data).encode()
+    return struct.pack("I{}s".format(len(payload)), destination, payload)
+
+
+def decode_payload(data):
+    (destination,), payload = struct.unpack("I", data[:4]), data[4:]
+
+    # If we want to support JSON payloads *AND* we want to decode them in python, uncomment below
+    # try:
+    #     payload = payload.decode()
+    #     payload = json.loads(payload)
+    # except JSONDecodeError:
+    #     raise DecodeError("Failed to decode payload {}".format(payload))
+
+    return destination, payload
+
+
+class DecodeError(Exception):
+    def __init(self, message=""):
+        super().__init__(message)
---
+++ Backend-API/api/controllers/controller.py
@@ -0,0 +1,78 @@
+import http.client
+import json
+import logging
+from json import JSONDecodeError
+import tornado.web
+import tornado.websocket
+import api
+
+
+class Controller(tornado.web.RequestHandler):
+    """
+    Controller
+
+    Attributes:
+        app (Result): The app instance
+        json_args (Dict): JSON Data
+    """
+
+    route = None
+
+    def __init__(self, application, request, **kwargs):
+        self.app = api.API.app
+        self.json_args = None
+        super().__init__(application, request, **kwargs)
+
+    def prepare(self):
+        headers = self.request.headers
+        self.json_args = {}
+        if "Content-Type" in headers and headers["Content-Type"].startswith("application/json"):
+            try:
+                self.json_args = json.loads(self.request.body)
+            except JSONDecodeError as e:
+                logging.warning(e.msg)
+                raise tornado.web.HTTPError(400, reason="Could not parse JSON data.")
+
+    def write_error(self, status_code, **kwargs):
+        info = kwargs.get("exc_info")
+        try:
+            if info is not None and len(info) > 2 and hasattr(info[1], "reason") and info[1].reason is not None:
+                self.write({"message": info[1].reason})
+            else:
+                self.write({"message": http.client.responses[status_code]})
+        except TypeError:
+            self.write({"message": http.client.responses[status_code]})
+
+    def write(self, chunk, set_content_type=True, status_code=None):
+        if isinstance(chunk, (dict, list)):
+            chunk = json.dumps(chunk)
+        if status_code is not None:
+            self.set_status(status_code)
+        if set_content_type:
+            self.set_header('Content-Type', 'application/json')
+        super().write(chunk)
+
+
+class WebSocketController(tornado.websocket.WebSocketHandler):
+    """
+    SwimSuite Controller
+
+    Attributes:
+        app (Result): The app instance
+    """
+
+    route = None
+
+    def __init__(self, application, request, **kwargs):
+        self.app = api.API.app
+        super().__init__(application, request, **kwargs)
+
+    def check_origin(self, origin):
+        if __debug__:
+            return True
+        return super().check_origin(origin)
+
+    def write_message(self, message, binary=False):
+        if isinstance(message, (dict, list)):
+            message = json.dumps(message)
+        super().write_message(message, binary)
