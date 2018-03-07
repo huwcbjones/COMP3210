c794aebd90e8f8b75b0c364902e1881108c1195d
Huw Jones <huwcbjones@gmail.com>
2018-03-07 16:59:14 +0000

    exit on fail

--- Backend-API/install.sh
+++ Backend-API/install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+set -o errexit
+
 python3 -m ensurepip
 python3 -m pip install pipenv
 pipenv --three install
 
+rm -f /etc/systemd/system/COMP3210-Backend.service
 ln -s COMP3210-Backend.service /etc/systemd/system/
 systemctl daemon-reload
