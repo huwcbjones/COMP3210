a6507082185a271661428b14ecc33890c89f56c3
Huw Jones <huwcbjones@gmail.com>
2018-03-07 16:53:07 +0000

    Moved service file
    Made install more defensive

--- Backend-API/install.sh
+++ Backend-API/install.sh
@@ -2,3 +2,6 @@
 python3 -m ensurepip
 python3 -m pip install pipenv
 pipenv --three install
+
+ln -s COMP3210-Backend.service /etc/systemd/system/
+systemctl daemon-reload
--- install.sh
+++ install.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env bash
 
+set -o errexit
+
 if [ "$EUID" -ne 0 ]
   then echo "This script requires root"
   exit
 fi
 
-useradd -r -U comp3210
+if id comp3210 >/dev/null 2>&1; then
+    echo "Not creating user as it already exists"
+else
+    echo "Creating user comp3210"
+    useradd -r -U comp3210
+fi
 
+echo "Cloning repo..."
 cd /opt/
 git clone git@github.com:huwcbjones/COMP3210.git
 
+echo "Setting permissions"
 chown -r comp3210:comp3210 COMP3210
 cd COMP3210
 
+echo "Installing backend API"
 cd /opt/COMP3210/Backend-API
 ./install.sh
 
+echo "Installing frontend"
 cd /opt/COMP3210/Frontend
 ./install.sh
-
-ln -s /opt/COMP3210/COMP3210-Backend.service /etc/systemd/system/
-systemctl daemon-reload
