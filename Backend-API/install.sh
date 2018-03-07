90d21d839b742e2e62b077db7d23346e9adbb9a2
Huw Jones <huwcbjones@gmail.com>
2018-03-07 17:51:40 +0000

    removed pip check

--- Backend-API/install.sh
+++ Backend-API/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -o errexit
 
-python3 -m ensurepip
 python3 -m pip install pipenv
 pipenv --three install
 
--- Frontend/install.sh
+++ Frontend/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -o errexit
-python3 -m ensurepip
+
 python3 -m pip install pipenv
 pipenv --three install
