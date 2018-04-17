43fbf37f6bf0634d88c6959fcc85d07e41df964d
Huw Jones <huwcbjones@gmail.com>
2018-04-17 18:41:30 +0100

    Added Discover/Node controllers

---
+++ Backend-API/api/controllers/discover.py
@@ -0,0 +1,27 @@
+from . import Controller
+from tornado.web import HTTPError
+
+class DiscoverController(Controller):
+
+    route = [r"/discover"]
+
+    async def get(self):
+        self.write({n.addr: n.identifier for _, n in self.app.sensor_net._slave_nodes.items()})
+
+class RediscoverController(Controller):
+
+    route = [r"/rediscover(/[0-9]*|)"]
+
+    async def get(self, time):
+        if time is None:
+            time = 5
+        elif isinstance(time, str):
+            if time == '':
+                time = 5
+            else:
+                try:
+                    time = int(time)
+                except ValueError:
+                    raise HTTPError(status_code=400, reason='Invalid argument for time "{}"'.format(time))
+        self.app.sensor_net.discover(time)
+        self.write({n.addr: n.identifier for _, n in self.app.sensor_net._slave_nodes.items()})
---
+++ Backend-API/api/controllers/node.py
@@ -0,0 +1,83 @@
+from . import Controller
+from sensor_net import Node, ProtocolError
+from tornado.web import HTTPError
+
+
+class NodeIOController(Controller):
+
+    route = [r"/node/([0-9]+)/io"]
+
+    async def get(self, node_id):
+        try:
+            node_id = int(node_id)
+        except ValueError:
+            raise HTTPError(status_code=404, reason="Node not found.")
+
+        if node_id not in self.app.sensor_net._slave_nodes:
+            raise HTTPError(status_code=404, reason="Node not found.")
+
+        node = self.app.sensor_net._slave_nodes[node_id]
+        node_info = {"id": node.addr, "identifier": node.identifier, "types": []}
+
+        payloads = self.app.sensor_net.get_node_io(node)
+        for payload in payloads.keys():
+            type = {}
+            node_info["types"].append(type)
+            type["type"] = payload.value
+            params = []
+            type["params"] = params
+            for i in range(0, payloads[payload]):
+                payload_info = self.app.sensor_net.get_payload_info(node, payload, index=i)
+                params.append({
+                    "id": i,
+                    "type": payload_info.decode()
+                })
+        self.write(node_info)
+
+
+class NodeSendDataController(Controller):
+
+    route = [r"/node/([0-9]+)"]
+
+    async def post(self, node_id):
+        try:
+            node_id = int(node_id)
+        except ValueError:
+            raise HTTPError(status_code=404, reason="Node not found.")
+
+        if node_id not in self.app.sensor_net._slave_nodes:
+            raise HTTPError(status_code=404, reason="Node not found.")
+
+        node = self.app.sensor_net._slave_nodes[node_id]
+
+        if "payload" not in self.json_args:
+            raise HTTPError(status_code=400, reason="payload not provided")
+        if "data" not in self.json_args:
+            raise HTTPError(status_code=400, reason="data not provided")
+
+        if not isinstance(self.json_args["data"], list):
+            raise HTTPError(status_code=400, reason="Invalid argument for data")
+
+        try:
+            payload = Node.Payload(int(self.json_args["payload"]))
+        except ValueError:
+            raise HTTPError(status_code=400, reason="Invalid payload type")
+
+        data = []
+        for d in self.json_args["data"]:
+            if "index" not in d:
+                raise HTTPError(status_code=400, reason="index not provided in data")
+            if "data" not in d:
+                raise HTTPError(status_code=400, reason="data not provided in data")
+
+            data.append(d)
+
+        for d in data:
+            try:
+                self.app.sensor_net.send_data(node, payload, d["data"], d["index"])
+            except ValueError or TypeError as e:
+                raise HTTPError(status_code=400, reason=e)
+            except ProtocolError as e:
+                raise HTTPError(400, reason=e.args[0])
+
+        self.set_status(200)
